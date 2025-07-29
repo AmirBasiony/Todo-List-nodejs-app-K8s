2b16203537e9383ba9df707c816587715d0a794a
AmirBasiony <amirbasiony14@gmail.com>
2025-07-29 21:26:37 +0300

    building the infrastructure

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -51,7 +51,12 @@ resource "aws_security_group" "WebTrafficSG_K8s" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
